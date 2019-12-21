b20e1827af8eea92734ded96cdb6299a1be98749
Alex <alex.lusco@gmail.com>
2019-12-20 22:00:56 -0800

    Add humanize duration to fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,6 +5,7 @@ set PATH $PATH ~/bin
 if not functions -q fundle; eval (curl -sfL https://git.io/fundle-install); end
 fundle plugin 'danhper/fish-ssh-agent'
 fundle plugin 'franciscolourenco/done'
+fundle plugin 'fishpkg/fish-humanize-duration'
 fundle init
 
 # Other Initialization
